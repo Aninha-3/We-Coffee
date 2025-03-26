f97f7720383c273755d5f965a7b77af3463fde81
Aninha-3 <anacbrito2007@gmail.com>
2025-03-26 09:47:19 -0300

    Mudança de titulo

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,7 +5,7 @@ export default function index(){
         <View style ={styles.container}>
             <View style= {styles.header}>
             <Image style={styles.headerImage} source={require("@/assets/images/chamadefogo.jpg")}/>
-            <Text style={styles.restauranteName}>Chama de Fogo</Text>
+            <Text style={styles.restauranteName}>We coffe</Text>
             <Text style={styles.subtitle}> A melhor churrascaria do brasil </Text>
             </View>
               
