74fd92e3dc7074bebc2796192c0413f0286e8620
Aninha-03 <anacbrito2007@gmail.com>
2025-03-27 08:45:40 -0300

    Mudando a cor peincipal do site

--- src/app/index/style.ts
+++ src/app/index/style.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container : {
         flex: 1,
-        backgroundColor: "#F2F2F2"
+        backgroundColor: "#F22F2"
     },
     header:{
     padding: 16
